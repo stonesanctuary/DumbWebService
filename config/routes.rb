f093b490b6f44dee1f8d764dab4310fbb4dc1527
Kevin Loken <kevinloken@me.com>
2011-07-26 16:02:33 -0700

    removed default rails pages, fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Fizzbits::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
   get "pages/home"
   get "pages/about"
   get "pages/contact"
@@ -56,7 +60,8 @@ Fizzbits::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+  root :to => 'pages#home'
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
